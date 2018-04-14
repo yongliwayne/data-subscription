aa3edba0edb14e2237da9e7e480aa587a4ab53ad
applezjm <1400012716@pku.edu.cn>
2018-04-14 11:03:26 -0700

    add testcase

--- ccws/test/test_gemini.py
+++ ccws/test/test_gemini.py
@@ -1,12 +1,72 @@
 from unittest import TestCase
 import csv
 import gzip
-
+import json
+import time
+import ccws
 
 class TestGemini(TestCase):
-    def test_write_data_csv(self):
-        with gzip.open('gemini.book.csv.gz', 'rt') as fn1, \
-                gzip.open('gemini.test.book.csv.gz', 'rt') as fn2:
+
+    def test(self, typ, fn):
+        if typ == 'gemini':
+            ex = ccws.Gemini()
+            ex.set_market('BTC/USD', 'order')
+            self.write_into_redis(ex, 'gemini_data')
+        elif typ == 'gdax_book':
+            ex = ccws.Gdax()
+            ex.set_market('BTC/USD', 'order')
+            self.write_into_redis(ex, 'gdax_book')
+        elif typ == 'gdax_trade':
+            ex = ccws.Gdax()
+            ex.set_market('BTC/USD', 'ticker')
+            self.write_into_redis(ex, 'gdax_trade')
+        self.process_data(ex)
+        self.write_into_csv(ex, fn)
+
+
+    def write_into_redis(self, ex, fn):
+        #ex = ccws.Gemini()
+        #ex.set_market('BTC/USD', 'order')
+        ex.connect_redis()
+        rdk = ex.Config.get('RedisCollectKey')
+        fd = open(fn, 'r')
+        for msg in fd:
+            if msg == 'open\n' or msg == 'close\n':
+                continue
+            ex.RedisConnection.lpush(rdk, json.dumps([1, msg]))
+        fd.close()
+
+    def process_data(self, ex):
+        #ex = ccws.Gemini()
+        #ex.set_market('BTC/USD', 'order')
+        ex.process_data()
+
+    def write_into_csv(self, ex, fn):
+        #ex = ccws.Gemini()
+        #ex.set_market('BTC/USD', 'order')
+        ex.connect_redis()
+        rdk = ex.Config.get('RedisOutputKey')
+        while True:
+            if ex.RedisConnection.llen(rdk) > 0:
+                data = json.loads(ex.RedisConnection.rpop(rdk).decode('utf8'))
+                with open(fn, 'a+') as csvFile:
+                    csvwriter = csv.writer(csvFile)
+                    csvwriter.writerow(data)
+            else:
+                time.sleep(60)
+
+    def diff(self, typ, fn):
+        if typ == 'gemini':
+            origin_fn = 'gemini_origin.csv'
+        elif typ == 'gdax_book':
+            origin_fn = 'gdax_book_origin.csv'
+        elif typ == 'gdax_trade':
+            origin_fn = 'gdax_trade_origin.csv'
+        self.test_write_data_csv(fn, origin_fn)
+
+    def test_write_data_csv(self, fn, origin_fn):
+        with open(fn, 'rt') as fn1, \
+                open(origin_fn, 'rt') as fn2:
             reader1 = csv.DictReader(fn1)
             reader2 = csv.DictReader(fn2)
             for row1 in reader1:
