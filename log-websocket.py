bc26dff12c6c948fa339b0755398dea4601fb48c
applezjm <1400012716@pku.edu.cn>
2018-04-25 01:37:48 -0700

    binary search for update book in base

---
+++ log-websocket.py
@@ -0,0 +1,39 @@
+import ccws
+import argparse
+
+
+class GetData:
+    @staticmethod
+    def collect(ex):
+        ex.connect_redis()
+        ex.collect_data()
+
+    @staticmethod
+    def write(ex):
+        ex.connect_redis()
+        # file name need to be changed
+        fn = open('input.txt', 'w')
+        input_key = ex.Config['RedisCollectKey']
+        length = ex.RedisConnection.llen(input_key)
+        for i in range(length):
+            fn.writelines(ex.RedisConnection.lrange(input_key, -i-1, -i-1)[0].decode('utf-8'))
+            fn.write('\n')
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Archive market data from WebSocket API.")
+    parser.add_argument('-e', '--exchange', metavar='exchange', required=True, help='exchange id.')
+    parser.add_argument('-s', '--symbol', metavar='symbol', required=True, help='symbol of exchange.')
+    parser.add_argument('-m', '--mode', metavar='mode', required=True, help='market mode.')
+    parser.add_argument('-f', '--function', metavar='function', required=True, help='collect/write data.')
+    args = parser.parse_args()
+
+    ex, currency_pair, mode, func = args.exchange, args.symbol, args.mode, args.function
+
+    ex = getattr(ccws, ex)()
+    ex.set_market(currency_pair, mode)
+    getattr(GetData, func)(ex)
+
+
+if __name__ == '__main__':
+    main()
