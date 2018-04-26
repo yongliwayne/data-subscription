38844f68fede45405805e16d0c5897a97ab92223
applezjm <1400012716@pku.edu.cn>
2018-04-26 07:40:21 -0700

    del htmltestrunner & verify reporter format

--- reporter.py
+++ reporter.py
@@ -1,52 +1,56 @@
 # -*- coding: UTF-8 -*-
-from HTMLTestRunner import HTMLTestRunner
 import unittest
 import time
+import datetime
 import os
 import smtplib
 from email.mime.text import MIMEText
-from email.utils import formataddr
+from email.utils import COMMASPACE
+
 
 my_sender = 'applezjm123456@126.com'
 my_pass = 'applezjm123'
-my_user = 'applezjm123456@126.com'
+my_user = ['yongliwang2014@gmail.com', '1400012716@pku.edu.cn']
 
 
-def SendReport(file_new):
-    with open(file_new, 'rb') as f:
-        mail_body = f.read()
-    msg = MIMEText(mail_body, 'html', 'utf-8')
-    msg['From'] = formataddr(["sender", my_sender])
-    msg['To'] = formataddr(["user", my_user])
-    msg['Subject'] = "report in testcase-1"
+def SendReport(mail_body):
+    msg = MIMEText(mail_body, 'plain', 'utf-8')
+    msg['From'] = my_sender
+    msg['To'] = COMMASPACE.join(my_user)
+    tmr = datetime.datetime.fromtimestamp(time.time())
+    msg['Subject'] = 'report in testcase-1 %4d/%02d/%02d' % (tmr.year, tmr.month, tmr.day)
 
     server = smtplib.SMTP_SSL("smtp.126.com", 465)
     server.login(my_sender, my_pass)
-    server.sendmail(my_sender, [my_user, ], msg.as_string())
+    server.sendmail(my_sender, my_user, msg.as_string())
     server.quit()
 
 
-def NewReport(TestReport):
-    lists = os.listdir(TestReport)
-    lists2 = sorted(lists)
-    file_new = os.path.join(TestReport, lists2[-1])
-    print(file_new)
-    return file_new
-
-
 if __name__ == '__main__':
-    test_dir = '/home/data/testcase/data-subscription/'
-    test_report = '/home/data/testcase/report/'
+    test_dir = '%s/testcase/data-subscription/' % os.path.expanduser('~')
 
-    discover = unittest.defaultTestLoader.discover(test_dir, pattern='test_*.py')
+    discover = unittest.defaultTestLoader.discover(test_dir, pattern='test_gemini.py')
 
-    now = time.strftime('%Y%m%d %H%M%S')
-    filename = test_report + '\\' + now + 'result.html'
-    fp = open(filename, 'wb')
-    runner = HTMLTestRunner(stream=fp, title='测试报告', description='用例执行情况：')
-    runner.run(discover)
-    fp.close()
-
-    new_report = NewReport(test_report)
-    SendReport(new_report)
+    runner = unittest.TextTestRunner()
+    res = runner.run(discover)
+    all_num = res.testsRun
+    fail_num = len(res.failures)
+    reason = res.failures
+    message = """
+    All case number
+    ...................
+    %d
+    Failed case number
+    ...................
+    %d
+    Failed case and reason
+    """ % (all_num, fail_num)
+    for i in reason:
+        message_add = """
+        .......................
+        %s
+        """ % str(i)
+        message += message_add
+    if len(res.failures) != 0:
+        SendReport(message)
 
