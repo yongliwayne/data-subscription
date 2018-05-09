9bd5258690f52890c5a5070011794a25bddfb14a
applezjm <1400012716@pku.edu.cn>
2018-05-09 00:07:07 -0700

    precision and timestamp

--- check_bas.py
+++ check_bas.py
@@ -13,7 +13,7 @@ def price_cmp(p1, p2, precision):
     return abs(float(p1) - float(p2)) > precision
 
 
-def main():
+def main(precision, timestamp):
     load_logger_config('BTC_USD_check_bas_test')
     logger = logging.getLogger('BTC_USD_check_bas_test')
     yesterday = datetime.datetime.fromtimestamp(time.time(), TIMEZONE) + datetime.timedelta(days=-1)
@@ -31,14 +31,14 @@ def main():
         row2 = reader2.__next__()
         try:
             while True:
-                p_ts1, ts1 = int(p_row1.get('timestamp', 0)), int(row1['timestamp'])
-                p_ts2, ts2 = int(p_row2.get('timestamp', 0)), int(row2['timestamp'])
+                p_ts1, ts1 = int(p_row1.get(timestamp, 0)), int(row1[timestamp])
+                p_ts2, ts2 = int(p_row2.get(timestamp, 0)), int(row2[timestamp])
                 bid1 = row1['bidp0']
                 bid2 = row2['bidp0']
                 ask1 = row1['askp0']
                 ask2 = row2['askp0']
                 if ts1 == ts2:
-                    if price_cmp(bid1, bid2) or price_cmp(ask1, ask2):
+                    if price_cmp(bid1, bid2, precision) or price_cmp(ask1, ask2, precision):
                         logger.info('%s %s' % (str(row1[i] for i in inf),
                                                str(row2[i] for i in inf)))
                     p_row1 = row1
@@ -56,7 +56,8 @@ def main():
                     continue
                 elif p_ts2 < ts1 < ts2:
                     closer = p_row2 if 2 * ts1 < (p_ts2 + ts2) else row2
-                    if price_cmp(bid1, closer.get('bidp0', 0)) or price_cmp(ask1, closer.get('askp0', 0)):
+                    if price_cmp(bid1, closer.get('bidp0', 0, precision))\
+                            or price_cmp(ask1, closer.get('askp0', 0, precision)):
                         logger.info('%s %s' % (str(row1[i] for i in inf),
                                                str(closer[i] for i in inf)))
                     p_row1 = row1
@@ -64,7 +65,8 @@ def main():
                     continue
                 elif p_ts1 < ts2 < ts1:
                     closer = p_row1 if 2 * ts2 < (p_ts1 + ts1) else row1
-                    if price_cmp(bid2, closer.get('bidp0', 0)) or price_cmp(ask2, closer.get('askp0', 0)):
+                    if price_cmp(bid2, closer.get('bidp0', 0, precision))\
+                            or price_cmp(ask2, closer.get('askp0', 0, precision)):
                         logger.info('%s %s' % (str(closer[i] for i in inf),
                                                str(row2[i] for i in inf)))
                     p_row2 = row2
@@ -74,4 +76,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    main(precision = 0.01, timestamp = 'timestamp')
