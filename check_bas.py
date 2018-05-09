cd3c3f2f0f32d8b89ab9babcb28c2fa2e69d9c61
applezjm <1400012716@pku.edu.cn>
2018-05-08 22:39:26 -0700

    while loop

--- check_bas.py
+++ check_bas.py
@@ -2,33 +2,52 @@ import gzip
 import csv
 
 
+def price_cmp(p1, p2):
+    return abs(float(p1) - float(p2)) < 0.01/2
+
+
 def main():
     fn1 = '/home/applezjm/trade_test/BTC_USD-gdax.book.csv.gz'
     fn2 = '/home/applezjm/trade_test/BTC_USD-gemini.book.csv.gz'
     with gzip.open(fn1, 'rt') as f1, gzip.open(fn2, 'rt') as f2:
         reader1 = csv.DictReader(f1)
         reader2 = csv.DictReader(f2)
-        last_row2 = reader2.__next__()
-        for row1 in reader1:
-            ts1 = int(row1['timestamp'])
-            spread1 = float(row1['askp0']) - float(row1['bidp0'])
-            for row2 in reader2:
-                last_ts2 = int(last_row2['timestamp'])
-                ts2 = int(row2['timestamp'])
-                if last_ts2 <= ts1 <= ts2:
-                    if ts1 *2 == (last_ts2 + ts2):
-                        spread2 = float(last_row2['askp0']) - float(last_row2['bidp0'])
-                        if abs(spread1 - spread2) > 0.01 / 2:
-                            print(row1, last_row2)
-                        spread2 = float(row2['askp0']) - float(row2['bidp0'])
-                        if abs(spread1-spread2) > 0.01 / 2:
-                            print(row1, row2)
-                    else:
-                        closer = last_row2 if ts1 * 2 < (last_ts2 + ts2) else row2
-                        spread2 = float(closer['askp0']) - float(closer['bidp0'])
-                        if abs(spread1-spread2) > 0.01 / 2:
-                            print(row1, closer)
-                last_row2 = row2
+        p_row1 = reader1.__next__()
+        p_row2 = reader2.__next__()
+        row1 = reader1.__next__()
+        row2 = reader2.__next__()
+        try:
+            while True:
+                p_ts1, ts1 = int(p_row1['timestamp']), int(row1['timestamp'])
+                p_ts2, ts2 = int(p_row2['timestamp']), int(row2['timestamp'])
+                bid1 = row1['bidp0']
+                bid2 = row2['bidp0']
+                ask1 = row1['askp0']
+                ask2 = row2['askp0']
+                if ts1 < p_ts2:
+                    p_row1 = row1
+                    row1 = reader1.__next__()
+                elif ts2 < p_ts1:
+                    p_row2 = row2
+                    row2 = reader2.__next__()
+                elif p_ts2 <= ts1 <= ts2:
+                    closer = p_row2 if 2 * ts1 < (p_ts2 + ts2) else row2
+                    if not price_cmp(bid1, closer['bidp0']):
+                        print('bid difference', row1, closer)
+                    if not price_cmp(ask1, closer['askp0']):
+                        print('ask difference', row1, closer)
+                    p_row1 = row1
+                    row1 = reader1.__next__()
+                elif p_ts1 <= ts2 <= ts1:
+                    closer = p_row1 if 2 * ts2 < (p_ts1 + ts1) else row1
+                    if not price_cmp(bid2, closer['bidp0']):
+                        print('bid difference', row2, closer)
+                    if not price_cmp(ask2, closer['askp0']):
+                        print('ask difference', row2, closer)
+                    p_row2 = row2
+                    row2 = reader2.__next__()
+        except StopIteration:
+            exit()
 
 
 if __name__ == '__main__':
