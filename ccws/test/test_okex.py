49cb596407a908973a4dfeea6e30c94883d2e301
applezjm <1400012716@pku.edu.cn>
2018-06-26 16:36:43 +0800

    add okex bch&eth

--- ccws/test/test_okex.py
+++ ccws/test/test_okex.py
@@ -62,3 +62,111 @@ class TestOkex(Test, Okex):
         fn1 = origin['Output']
         fn2 = '%s/%s/%s' % (HOME_PATH, origin['Date'], self.Config['FileName'])
         self.compare_two_csv(fn1, fn2)
+
+    def test_BCH_USDT_order(self):
+        origin = {
+            'FileName': 'BCH_USDT-okex_order.gz',
+            'Date': '2018/06/26',
+            'Output': 'BCH_USDT-okex.book.csv.gz',
+        }
+        self.initialization('BCH/USDT', 'order', origin['Date'])
+
+        input_key = self.Config['RedisCollectKey']
+        self.write_into_redis(input_key, self.RedisConnection, origin['FileName'])
+
+        try:
+            with timeout(60, exception=RuntimeWarning):
+                self.process_data()
+        except RuntimeWarning:
+            pass
+
+        try:
+            with timeout(60, exception=RuntimeWarning):
+                self.write_data_csv()
+        except RuntimeWarning:
+            pass
+
+        fn1 = origin['Output']
+        fn2 = '%s/%s/%s' % (HOME_PATH, origin['Date'], self.Config['FileName'])
+        self.compare_two_csv(fn1, fn2)
+
+    def test_BCH_USDT_ticker(self):
+        origin = {
+            'FileName': 'BCH_USDT-okex_ticker.gz',
+            'Date': '2018/06/26',
+            'Output': 'BCH_USDT-okex.trade.csv.gz',
+        }
+        self.initialization('BCH/USDT', 'trade', origin['Date'])
+
+        input_key = self.Config['RedisCollectKey']
+        self.write_into_redis(input_key, self.RedisConnection, origin['FileName'])
+
+        try:
+            with timeout(30, exception=RuntimeWarning):
+                self.process_data()
+        except RuntimeWarning:
+            pass
+
+        try:
+            with timeout(30, exception=RuntimeWarning):
+                self.write_data_csv()
+        except RuntimeWarning:
+            pass
+
+        fn1 = origin['Output']
+        fn2 = '%s/%s/%s' % (HOME_PATH, origin['Date'], self.Config['FileName'])
+        self.compare_two_csv(fn1, fn2)
+
+    def test_ETH_USDT_order(self):
+        origin = {
+            'FileName': 'ETH_USDT-okex_order.gz',
+            'Date': '2018/06/26',
+            'Output': 'ETH_USDT-okex.book.csv.gz',
+        }
+        self.initialization('ETH/USDT', 'order', origin['Date'])
+
+        input_key = self.Config['RedisCollectKey']
+        self.write_into_redis(input_key, self.RedisConnection, origin['FileName'])
+
+        try:
+            with timeout(60, exception=RuntimeWarning):
+                self.process_data()
+        except RuntimeWarning:
+            pass
+
+        try:
+            with timeout(60, exception=RuntimeWarning):
+                self.write_data_csv()
+        except RuntimeWarning:
+            pass
+
+        fn1 = origin['Output']
+        fn2 = '%s/%s/%s' % (HOME_PATH, origin['Date'], self.Config['FileName'])
+        self.compare_two_csv(fn1, fn2)
+
+    def test_ETH_USDT_ticker(self):
+        origin = {
+            'FileName': 'ETH_USDT-okex_ticker.gz',
+            'Date': '2018/06/26',
+            'Output': 'ETH_USDT-okex.trade.csv.gz',
+        }
+        self.initialization('ETH/USDT', 'trade', origin['Date'])
+
+        input_key = self.Config['RedisCollectKey']
+        self.write_into_redis(input_key, self.RedisConnection, origin['FileName'])
+
+        try:
+            with timeout(30, exception=RuntimeWarning):
+                self.process_data()
+        except RuntimeWarning:
+            pass
+
+        try:
+            with timeout(30, exception=RuntimeWarning):
+                self.write_data_csv()
+        except RuntimeWarning:
+            pass
+
+        fn1 = origin['Output']
+        fn2 = '%s/%s/%s' % (HOME_PATH, origin['Date'], self.Config['FileName'])
+        self.compare_two_csv(fn1, fn2)
